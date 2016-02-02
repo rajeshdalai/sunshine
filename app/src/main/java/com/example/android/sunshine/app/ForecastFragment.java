e313a0a0c9c77a66491549c7aa7c2f180608dd7b
Dalai <505503@CTS.COM>
2016-02-02 18:24:36 +0530

    Json Parsing Done

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -2,8 +2,10 @@ package com.example.android.sunshine.app;
 
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,12 +16,17 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -85,6 +92,79 @@ public class ForecastFragment extends Fragment {
     public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        /**
+         * Prepare date in a Readable Format
+         * @param time
+         * @return shortenedDateFormat
+         */
+        private String getReadableDateString(long time){
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(time);
+        }
+
+        /**
+         * Prepare high and low values for presentation
+         * @param high
+         * @param low
+         * @return highLowStr
+         */
+        private String formatHighLows(double high, double low){
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            String highLowStr = roundedHigh+"/"+roundedLow;
+            return highLowStr;
+        }
+
+        private String[] getWeatherDataFromJSON(String forecastJsonStr , int numDays) throws JSONException {
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_MIN = "min";
+            final String OWM_MAX = "max";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(),dayTime.gmtoff);
+
+            dayTime = new Time();
+
+            String[] resultStrs = new String[numDays];
+
+            for (int i = 0; i < weatherArray.length() ; i++) {
+
+                String day;
+                String description;
+                String highAndLow;
+
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                long dateTime;
+                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                day = getReadableDateString(dateTime);
+
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = dayForecast.getDouble(OWM_MAX);
+                double low = dayForecast.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high,low);
+                resultStrs[i] = day+" - "+description+" - "+highAndLow;
+            }
+
+            for (String s:resultStrs) {
+                Log.v(LOG_TAG,"Forecast entry: "+s);
+            }
+            return resultStrs;
+        }
 
         @Override
         protected Void doInBackground(String... params) {
